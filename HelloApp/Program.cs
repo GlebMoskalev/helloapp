67ae758274db75cc9f8fda4afc7b17a65536d266
black <black@example.com>
2022-11-04 17:46:20 +0500

    hello everyone

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,6 +1,10 @@
 ﻿
+<<<<<<< HEAD
 Console.WriteLine("Hello, World!");
 static void greetWhite()
 {
     Console.WriteLine("hello white");
 }
+=======
+Console.WriteLine("Hello everyone");
+>>>>>>> 11b7818 (hello everyone)
